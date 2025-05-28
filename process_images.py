82f5a216d029b65ef666e84f34316060e0337ace
Leaves333 <canchenli@gmail.com>
2025-05-27 20:32:27 -0500

    save data array to file

--- process_images.py
+++ process_images.py
@@ -15,11 +15,14 @@ def resize_image(image_path: str):
         resized_image = img.resize(size=(128, 128));
         return resized_image
 
-# converts an image to an 3d array, containing the rgb values of each pixel
-def convert_image_to_array(img: Image.Image):
-    arr = np.zeros((img.size[0], img.size[1], 3), dtype=np.uint8)
-    for i in range(img.size[0]):
-        for j in range(img.size[1]):
+# converts an image to an 128x128x3 array, containing the rgb values of each pixel
+def convert_image_to_array(image_path: str):
+
+    img = resize_image(image_path)
+    arr = np.zeros((128, 128, 3), dtype=np.uint8)
+
+    for i in range(128):
+        for j in range(128):
 
             # NOTE: the code error doesn't actually cause problems
 
@@ -29,11 +32,24 @@ def convert_image_to_array(img: Image.Image):
             arr[i][j][2] = pixel[2]
     return arr
 
+def process_image_data():
+    image_paths = os.listdir("images")
+    arr = np.zeros((len(image_paths), 128, 128, 3), dtype=np.uint8)
+    for i in range(len(image_paths)):
+        path = image_paths[i]
+        arr[i] = convert_image_to_array(f"images/{path}")
+
+    np.save("image_data.npy", arr)
+    print("image data successfully saved!")
+    return arr
+
 IMAGE_PATH = "images/679cd0fa56e8296a174cc33dd99b1a36_51.jpg"
 
 print("process images running!")
-resized_image = resize_image(IMAGE_PATH)
-arr = convert_image_to_array(resized_image)
-print(arr)
+process_image_data()
 
+# resized_image = resize_image(IMAGE_PATH)
+# arr = convert_image_to_array(resized_image)
+# print(arr)
+#
 # print(get_id_from_filepath(IMAGE_PATH))
