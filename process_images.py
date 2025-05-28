850cd456ecf6a1fe31df8dd72e1644e362c2f171
Leaves333 <canchenli@gmail.com>
2025-05-28 17:19:52 -0500

    fix not being to store grayscale images

--- process_images.py
+++ process_images.py
@@ -18,6 +18,8 @@ def resize_image(image_path: str):
 # converts an image to an 128x128x3 array, containing the rgb values of each pixel
 def convert_image_to_array(image_path: str):
 
+    print(image_path)
+
     img = resize_image(image_path)
     arr = np.zeros((128, 128, 3), dtype=np.uint8)
 
@@ -27,9 +29,14 @@ def convert_image_to_array(image_path: str):
             # NOTE: the code error doesn't actually cause problems
 
             pixel: tuple[int, int, int]  = img.getpixel((i, j))
-            arr[i][j][0] = pixel[0]
-            arr[i][j][1] = pixel[1]
-            arr[i][j][2] = pixel[2]
+            if (type(pixel) == tuple):
+                arr[i][j][0] = pixel[0]
+                arr[i][j][1] = pixel[1]
+                arr[i][j][2] = pixel[2]
+            else:
+                arr[i][j][0] = pixel
+                arr[i][j][1] = pixel
+                arr[i][j][2] = pixel
     return arr
 
 def process_image_data():
