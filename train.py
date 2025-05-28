b1fb705bfefcba77d846d72ab4bebc22464a7bb0
Leaves333 <canchenli@gmail.com>
2025-05-28 18:09:28 -0500

    train the model (poorly)

--- train.py
+++ train.py
@@ -7,3 +7,24 @@ print("train.py running !!")
 print("tensorflow version: ", tf.version.VERSION)
 
 (train_images, train_tags), (test_images, test_tags) = utils.load_training_data()
+
+model = utils.create_model()
+print()
+print(model.summary())
+
+checkpoint_path = "training/model.weights.h5"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create a callback that saves the model's weights
+cp_callback = keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                             save_weights_only=True,
+                                             verbose=1)
+
+# Train the model with the new callback
+model.fit(train_images, 
+          train_tags,  
+          epochs=10,
+          validation_data=(test_images, test_tags),
+          callbacks=[cp_callback])  # Pass callback to training
+
+model.evaluate(test_images, test_tags)
--- utils.py
+++ utils.py
@@ -25,14 +25,31 @@ def create_table_if_it_doesnt_exist():
     cur.execute("""
         CREATE TABLE IF NOT EXISTS images (
             id INTEGER PRIMARY KEY,
-            hash TEXT,
-            tag_string_general TEXT,
+            hash TEXT, tag_string_general TEXT,
             tag_string_character TEXT,
             tag_string_copyright TEXT,
             tag_string_artist TEXT,
             tag_string_meta TEXT
         )
     """)
+
+def create_model() -> keras.Sequential:
+    model = keras.models.Sequential([
+        keras.layers.Input(shape=(128, 128, 3)),
+        keras.layers.Flatten(),
+        keras.layers.Dense(1024, activation='relu'),
+        keras.layers.Dense(128, activation='relu'),
+        keras.layers.Dropout(0.2),
+        keras.layers.Dense(5)
+    ])
+
+    model.compile(
+        optimizer='adam',
+        loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=['accuracy'],
+    )
+
+    return model
 
 def load_training_data():
 
